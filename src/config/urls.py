afaac9de7211fafd1856f97a7eee69d699849d98
Kinjal Christian <kinjal@Kinjals-MacBook-Pro.local>
2025-10-15 15:21:59 +0530

    CHORE: remove ReDoc, keep only Swagger UI for single API docs endpoint

--- src/config/urls.py
+++ src/config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,5 +31,4 @@ if settings.DEBUG:
     urlpatterns += [
         path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
         path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-        path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     ]
